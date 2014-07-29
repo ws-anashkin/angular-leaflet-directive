25f27990e87e8fae3838cd992b43920f040b9984
ws-anashkin <ws.anashkin@gmail.com>
2014-07-29 19:46:30 +0400

    New version

--- dist/angular-leaflet-directive.js
+++ dist/angular-leaflet-directive.js
@@ -94,9 +94,11 @@
           map.whenReady(function () {
             leafletData.setMap(map, attrs.id);
           });
+
           $rootScope.$on('triggerLeafletResize', function () {
             map._onResize();
           });
+          
           scope.$on('$destroy', function () {
             leafletData.unresolveMap(attrs.id);
           });
